e5edf4225248f4fd0da3ae4e894ccd75a43650ac
morpheusmmt <1maida.martins@gmail.com>
2025-10-21 11:27:15 -0300

    Solução final para ERR_REQUIRE_CYCLE_MODULE (Lazy Load de JWT)

--- backend/src/middlewares/authMiddleware.ts
+++ backend/src/middlewares/authMiddleware.ts
@@ -1,12 +1,13 @@
 import { Request, Response, NextFunction } from "express";
-import jwt from "jsonwebtoken";
 
 export const auth = (req: any, res: Response, next: NextFunction) => {
+  const jwt = require("jsonwebtoken"); 
+
   const bearer = req.headers.authorization;
   if (!bearer) return res.status(403).json({ error: "Token necessário" });
   const token = bearer.split(" ")[1];
   try {
-    req.user = jwt.verify(token, process.env.JWT_SECRET!);
+    req.user = jwt.verify(token, process.env.JWT_SECRET!); 
     next();
   } catch {
     res.status(401).json({ error: "Token inválido" });
